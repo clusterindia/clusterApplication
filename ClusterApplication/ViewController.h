0c71e5d352d5f66fbfcf01e852e0991abc2094a1
Harishakn <test@tests-Mac.local>
2016-10-21 03:34:54 -0200

    Initial project

---
+++ ClusterApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClusterApplication
+//
+//  Created by test on 10/21/16.
+//  Copyright © 2016 NeoRays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ClusterApplication/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ClusterApplication
+//
+//  Created by test on 10/21/16.
+//  Copyright © 2016 NeoRays. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
